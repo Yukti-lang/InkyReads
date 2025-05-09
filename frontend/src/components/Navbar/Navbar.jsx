8bc3403fa1282694db9ee4a5778a97bbd9ffc25d
DhingraPreeti <preetidhingra2318@gmail.com>
2025-05-09 14:39:43 +0530

    Frontend

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-amber-800 text-amber-200 px-8 py-2">
+        <div>
+            <h1 className="text-2xl font-semibold">InkyReads</h1></div>
+        <div>Links</div>
+      
+    </div>
+  )
+}
+
+export default Navbar
