ca51780c9ddc49a6a6e0bedab4da91a595ef221c
Yukti-lang <yuktichawla1901@gmail.com>
2025-05-10 13:10:10 +0530

    recently added

--- backend1/app.js
+++ backend1/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require ("cors");
 require('dotenv').config();
-require('./connection/conn');   
+require('./connection/conn');  
+
 const User = require('./routes/userauth');
 const Books = require('./routes/book');
 const Favourite = require('./routes/favourite');
 const Cart1 = require('./routes/cart');
 const Order = require('./routes/order');
+app.use(cors())
 app.use(express.json());
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
